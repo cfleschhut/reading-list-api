29a3c52ab3c746fa9efb35933ca626a14c0ac254
Christian Fleschhut <weltrekordhalter@gmail.com>
2015-12-04 23:57:16 +0100

    add book serializer & fix tests

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ActiveRecord::Base
-  scope :finished, -> { where"finished_at IS NOT NULL" }
+  scope :finished, -> { where("finished_at IS NOT NULL") }
 
   validates :title, presence: true
 end
