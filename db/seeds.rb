272cde9177e92a9cf118486d15a4a8048ed212e5
Christian Fleschhut <weltrekordhalter@gmail.com>
2015-12-05 01:39:55 +0100

    add genre serializer & update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,7 @@
-Book.create!(title: "Pragmatic Programmer", rating: 5)
-Book.create!(title: "Ender’s Game", rating: 4)
+Genre.destroy_all
+Genre.create!(name: "Programming")
+Genre.create!(name: "Science Fiction")
+
+Book.destroy_all
+Book.create!(title: "Pragmatic Programmer", rating: 5, author: "Dave Thomas", genre: Genre.first)
+Book.create!(title: "Ender’s Game", rating: 4, author: "Orson Scott Card", genre: Genre.last)
